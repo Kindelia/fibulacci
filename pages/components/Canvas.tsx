52162aef41597545051db6edeeadb300b8a887fa
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-09-23 18:51:46 -0300

    chore: add requestAnimationFrame

--- pages/components/Canvas.tsx
+++ pages/components/Canvas.tsx
@@ -2,21 +2,28 @@ import { useEffect, useRef } from "react";
 
 export type CanvasProps = React.ComponentProps<"canvas"> & {
   draw: (context: CanvasRenderingContext2D) => void;
+  state: any;
 };
 
 export function Canvas(props: CanvasProps) {
-  const { draw, ...rest } = props;
+  const { draw, state, ...rest } = props;
 
   const ref = useRef();
 
-  useEffect(() => {
+  function tick() {
     const canvas = ref.current as HTMLCanvasElement;
     const context = canvas.getContext("2d");
 
     if (!context) return;
 
     draw(context);
-  }, []);
+
+    requestAnimationFrame(tick);
+  }
+  
+  useEffect(() => {
+   requestAnimationFrame(tick);
+  }, [state]);
 
   return <canvas ref={ref} {...rest} />;
 }
