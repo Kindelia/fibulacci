cbf678aa29a354a9b2a408bd3a71125b075b234a
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-09-30 13:55:12 -0300

    chore: addQueryClientProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
-
