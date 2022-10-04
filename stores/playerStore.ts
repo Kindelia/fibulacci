6103775bd5fa7cf8b41446b7b088bd5a8457f3bc
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-04 16:56:35 -0300

    chore: add exp, level, mp, hp, mpMax, hpMax

--- stores/playerStore.ts
+++ stores/playerStore.ts
@@ -1,21 +1,33 @@
 import { atom } from "nanostores";
 
 type Player = {
+  exp?: number;
+  hp?: number;
+  level?: number;
+  maxHp?: number;
+  maxMp?: number;
+  movements?: string[];
+  mp?: number;
   name?: string;
   score?: number;
   speed?: number;
   x?: number;
   y?: number;
-  movements?: string[];
 };
 
 const playerInitialState: Player = {
+  exp: 0,
+  hp: 100,
+  level: 1,
+  maxHp: 100,
+  maxMp: 100,
+  movements: [],
+  mp: 100,
   name: "Anony",
   score: 0,
   speed: 1,
   x: 0,
   y: 0,
-  movements: [],
 };
 
 export const playerStore = atom<Player>({
