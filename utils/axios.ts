45311fe65613fc3c63b8583891bd08d21a3e2ddc
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-04 16:56:43 -0300

    chore: rename client to api

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const urls: string[] = ["0.0.0.0", "64.227.110.69", "188.166.3.140"];
 
-export const client = axios.create({
+export const api = axios.create({
   baseURL: `http://${urls[0]}:8000`,
-  headers: {
-    // "Content-Type": "text/plain",
-    "Content-Type": "application/json",
-  },
 });
