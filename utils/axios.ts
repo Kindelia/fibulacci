2c3dab1285f62ae0659167ad4b87da5e8f34c4a2
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-04 18:25:37 -0300

    chore: update urls with https

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const urls: string[] = ["0.0.0.0", "64.227.110.69", "188.166.3.140"];
+const urls: string[] = [
+  "0.0.0.0",
+  "node-ams3.testnet.kindelia.org",
+  "node-sfo3.testnet.kindelia.org",
+];
 
 export const api = axios.create({
-  baseURL: `http://${urls[0]}:8000`,
+  baseURL: `https://${urls[1]}`,
 });
