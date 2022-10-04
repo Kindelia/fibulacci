556b21f6f92d4c58d950416cc375c14afabf1e44
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-04 16:54:52 -0300

    chore: remove unused hook

--- hooks/useKindeliaStats.tsx
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { client } from "../utils/axios";
-
-export function useKindeliaStats() {
-  return useQuery(["requestTest"], () =>
-    client.get("/stats").then((res) => res.data)
-  );
-}
