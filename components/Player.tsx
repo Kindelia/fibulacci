251fddf9730230e7b789fd7423f5de81349aeed4
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-04 16:54:33 -0300

    chore: add Window in Player

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,13 +1,16 @@
-import { useRef } from 'react';
-
-import { usePlayer } from '../hooks/usePlayer';
-import { Blob } from './Blob';
+import { usePlayer } from "../hooks/usePlayer";
+import { Window } from "./Window";
+import { Blob } from "./Blob";
 
 type PlayerProps = {};
 
 export function Player(_props: PlayerProps) {
   const player = usePlayer();
 
-
-  return <Blob side="right" x={player.x} y={player.y} />
+  return (
+    <>
+      <Blob side="right" x={player.x} y={player.y} />
+      <Window />
+    </>
+  );
 }
