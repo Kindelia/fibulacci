f830c31ced67f7057d3c4e115a8b1dc5008901ae
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-04 16:55:19 -0300

    chopre: remove useKey for Player

--- hooks/usePlayer.tsx
+++ hooks/usePlayer.tsx
@@ -3,12 +3,15 @@ import { useKey } from "react-use";
 import { playerStore, setPlayer } from "../stores/playerStore";
 
 export function usePlayer() {
-  const player = useStore(playerStore);
+  // const player = useStore(playerStore);
 
-  useKey("ArrowUp", () => setPlayer({ y: player.y - 1 }));
-  useKey("ArrowDown", () => setPlayer({ y: player.y + 1 }));
-  useKey("ArrowRight", () => setPlayer({ x: player.x + 1 }));
-  useKey("ArrowLeft", () => setPlayer({ x: player.x - 1 }));
+  // useKey("ArrowUp", () => setPlayer({ y: player.y - 1 }));
+  // useKey("ArrowDown", () => setPlayer({ y: player.y + 1 }));
+  // useKey("ArrowRight", () => setPlayer({ x: player.x + 1 }));
+  // useKey("ArrowLeft", () => setPlayer({ x: player.x - 1 }));
 
-  return player;
+  return {
+    x: 0,
+    y: 0,
+  }
 }
