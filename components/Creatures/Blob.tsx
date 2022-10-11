6ff062f9395bcfbe04b17d8257c9e3820da8327c
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-11 20:32:34 -0300

    chore: add blob voice

--- components/Creatures/Blob.tsx
+++ components/Creatures/Blob.tsx
@@ -1,18 +1,22 @@
+import useSound from "use-sound";
+
 export type BlobProps = {
   side: "left" | "right";
   x: number;
   y: number;
   scale?: number;
+  size?: number;
 };
 
 export function Blob(props: BlobProps) {
-  const size = 32;
-
-  const { side, x, y, scale } = {
+  const { side, x, y, scale, size } = {
     scale: 1,
     side: "right",
+    size: 32,
     ...props,
   };
+
+  const [play] = useSound("/sounds/blob-voice.mp3", { volume: 0.9 });
 
   return (
     <div
@@ -24,6 +28,7 @@ export function Blob(props: BlobProps) {
         top: y,
         left: x,
       }}
+      onClick={() => play()}
     />
   );
 }
