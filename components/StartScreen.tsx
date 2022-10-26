6ecc94d96488d08084fd1aadce5037471b0ef184
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-26 01:02:44 -0300

    chore: create goToPlay

--- components/StartScreen.tsx
+++ components/StartScreen.tsx
@@ -1,13 +1,23 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Router from "next/router";
 
-import { LogoIcon } from './LogoIcon';
+import { LogoIcon } from "./LogoIcon";
 
 export function StartScreen() {
+  function goToPlay() {
+    Router.push("/play");
+  }
+
   return (
     <div className="flex flex:col ai:center jc:center h:100vh bg:black gap:20">
       <LogoIcon className="h:40% @float|3s|ease-in-out|infinite" />
       <p>Welcome Fibulacci</p>
-      <button className='bg:white py:20 px:100 f:black rounded bg:gray-60:hover'>START</button>
+      <button
+        className="bg:white py:20 px:100 f:black rounded bg:gray-60:hover"
+        onClick={goToPlay}
+      >
+        START
+      </button>
       <ConnectButton />
       <p>CREDITS</p>
     </div>
