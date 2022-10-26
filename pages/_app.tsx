b9ca28e6bf9ba62b95eb065a01376bc2bd0bafb9
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-10-25 21:01:11 -0300

    chore: add metamask with rainbowkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,37 @@
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import "../styles/globals.css";
+import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "KChain",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={new QueryClient()}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <QueryClientProvider client={new QueryClient()}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
