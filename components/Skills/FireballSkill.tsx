24d23505c4623bb274c305a51d55621ddcec764f
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-11-05 22:25:40 -0300

    chore: create FireballSkill

---
+++ components/Skills/FireballSkill.tsx
@@ -0,0 +1,44 @@
+import $ from "@master/literal";
+import { useState } from "react";
+import useSound from "use-sound";
+
+export type FireballSkillProps = {
+  x: number;
+  y: number;
+};
+
+export function FireballSkill(props: FireballSkillProps) {
+  const { x, y } = props;
+
+  const [isPlayingSound, setIsPlayingSound] = useState(false);
+
+  const [play, { stop }] = useSound("/sounds/fireball.mp3");
+
+  function handlePlaySound() {
+    if (isPlayingSound) return;
+
+    try {
+      play();
+      setIsPlayingSound(true);
+    } catch (error) {
+      setIsPlayingSound(false);
+    } finally {
+      setTimeout(() => setIsPlayingSound(false), 2000);
+    }
+  }
+
+  return (
+    <div
+      onClick={handlePlaySound}
+      className={$`
+        bg:url('/images/skills/fireball.png') 
+        h:32 
+        w:32 
+        top:${y} 
+        left:${x} 
+        abs 
+        @fireball|2s|steps(8)|infinite
+      `}
+    />
+  );
+}
