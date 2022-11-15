7a393a1c73ecff14b446d56ccc6ae9977db2c926
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-11-15 18:24:25 -0300

    add rome.js (#3)
    * chore: add NODE_URL in env.ts
    
    * chore: remove eslint and add rome
    
    * chore: run format

--- stores/playerStore.ts
+++ stores/playerStore.ts
@@ -2,50 +2,50 @@ import { persistentAtom } from "@nanostores/persistent";
 import { FibObject } from "../hooks/useStateQuery";
 
 type Player = FibObject & {
-  level?: number;
+	level?: number;
 };
 
 const initialPlayerState: Player = {
-  type: "Player",
-  num: "",
-  y: "5",
-  sl: "",
-  s5: "",
-  s1: "",
-  xp_tot: "",
-  cd: "",
-  s7: "",
-  s3: "",
-  x: "5",
-  hp: "",
-  gd: "",
-  s6: "",
-  s2: "",
-  rt: "",
-  s0: "",
-  s4: "",
-  fat: "",
-  level: 1,
+	type: "Player",
+	num: "",
+	y: "5",
+	sl: "",
+	s5: "",
+	s1: "",
+	xp_tot: "",
+	cd: "",
+	s7: "",
+	s3: "",
+	x: "5",
+	hp: "",
+	gd: "",
+	s6: "",
+	s2: "",
+	rt: "",
+	s0: "",
+	s4: "",
+	fat: "",
+	level: 1,
 };
 
 export const playerStore = persistentAtom<Player>(
-  "player",
-  { ...initialPlayerState },
-  {
-    encode: JSON.stringify,
-    decode: JSON.parse,
-  }
+	"player",
+	{ ...initialPlayerState },
+	{
+		encode: JSON.stringify,
+		decode: JSON.parse,
+	},
 );
 
 export function setPlayer(player: Player) {
-  playerStore.set({
-    ...player,
-    level: Number(player.xp_tot) / 10,
-  });
+	playerStore.set({
+		...player,
+		level: Number(player.xp_tot) / 10,
+	});
 }
 
 export function resetPlayer() {
-  playerStore.set({
-    ...initialPlayerState,
-  });
+	playerStore.set({
+		...initialPlayerState,
+	});
 }
