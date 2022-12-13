040eec203789277b58f5a4dd8ee067b2e28761dd
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-12-12 21:32:41 -0300

    chore: add isEnableRangeSkill

--- components/Interfaces/WindowSlot.tsx
+++ components/Interfaces/WindowSlot.tsx
@@ -1,43 +1,52 @@
 import * as F from "@fxts/core";
 import $ from "@master/literal";
+import { useStore } from "@nanostores/react";
+import { gameStore, setGameStore } from "../../stores/gameStore";
 
 type WindowSlotProps = {};
 
 export function WindowSlot(_props: WindowSlotProps) {
-  const scale = 2.8;
-  const size = 32 * scale;
+	const scale = 2.8;
+	const size = 32 * scale;
 
-  return (
-    <div className="flex flex:row flex:wrap w:400 jc:center ai:center">
-      {F.pipe(
-        F.range(16),
-        F.map((i: number) => (
-          <div
-            className={$`
-              bg:url('/images/window-slot.png')
-              w:${size}
-              h:${size}
-              bg:cover
-              bg:no-repeat
-            `}
-            key={i}
-          >
-            {i === 0 ? (
-              <div
-                className={$`
-                bg:url('/images/fire-ball.png')
-                w:${size}
-                h:${size}
-                bg:cover
-                bg:no-repeat
-                cursor:pointer
-              `}
-              />
-            ) : null}
-          </div>
-        )),
-        F.toArray
-      )}
-    </div>
-  );
+	const game = useStore(gameStore);
+
+	return (
+		<div className="flex flex:row flex:wrap w:400 jc:center ai:center">
+			{F.pipe(
+				F.range(16),
+				F.map((i: number) => (
+					<div
+						className={$`
+							bg:url('/images/window-slot.png')
+							w:${size}
+							h:${size}
+							bg:cover
+							bg:no-repeat
+						`}
+						key={i}
+					>
+						{i === 0 ? (
+							<div
+								className={$`
+									bg:url('/images/fire-ball.png')
+									w:${size}
+									h:${size}
+									bg:cover
+									bg:no-repeat
+									cursor:pointer
+								`}
+								onClick={() => {
+									setGameStore({
+										isEnableRangeSkill: !game.isEnableRangeSkill,
+									});
+								}}
+							/>
+						) : null}
+					</div>
+				)),
+				F.toArray,
+			)}
+		</div>
+	);
 }
