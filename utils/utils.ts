b7c1dd6a8d67b07ba4d4943ac9d6d11cd73a73c8
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-12-12 21:34:16 -0300

    chore: create createDiamond and randomUuid

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,8 +1,31 @@
 export const sleep = (ms: number) =>
-  new Promise((resolve) => setTimeout(resolve, ms));
+	new Promise((resolve) => setTimeout(resolve, ms));
 
 export const formatAddress = (address: string): string =>
-  `#x${address.replace("0x", "").slice(0, 29)}`;
+	`#x${address.replace("0x", "").slice(0, 29)}`;
 
 export const getNumbList = (json: any): string[] =>
-  JSON.stringify(json).match(/(?<=numb":").*?(?="})/g);
+	JSON.stringify(json).match(/(?<=numb":").*?(?="})/g);
+
+export const randomUuid = (): string => {
+	return Array(8)
+		.fill(undefined)
+		.map(() =>
+			Math.floor((1 + Math.random()) * 0x10000)
+				.toString(16)
+				.substring(1),
+		)
+		.join("-");
+};
+
+export const createDiamond = (range: number): number[][] =>
+	Array(range * 2 + 1)
+		.fill(undefined)
+		.map((_, y) =>
+			Array(range * 2 + 1)
+				.fill(undefined)
+				.map((_, x) => {
+					const distance = Math.abs(x - range) + Math.abs(y - range);
+					return distance <= range ? 1 : 0;
+				}),
+		);
