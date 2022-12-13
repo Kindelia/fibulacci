19ecfc3d75828f48378d45b35611930fe2f90a6f
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-12-12 21:32:50 -0300

    chore: enable isEnableRangeSkill

--- components/Player/Player.tsx
+++ components/Player/Player.tsx
@@ -5,98 +5,96 @@ import { useInterval } from "react-use";
 
 import { usePlayerMoveMutation } from "../../hooks/useMoveMutation";
 import { FibObject } from "../../hooks/useStateQuery";
-import {
-  addGameStoreEvent,
-  gameStore,
-  setGameStore,
-} from "../../stores/gameStore";
+import { gameStore, setGameStore } from "../../stores/gameStore";
 import { Blob } from "../Creatures/Blob";
 import { Window } from "../Interfaces/Window";
-
-if (typeof window !== "undefined") {
-  window.addEventListener("click", (e) => {
-    addGameStoreEvent({
-      type: "fireball",
-      position: {
-        x: e.offsetX,
-        y: e.offsetY,
-      },
-      ms: 5000,
-    });
-  });
-}
+import { FireballRangeSkill } from "../Skills/FireballRangeSkill";
+import { FireballSkill } from "../Skills/FireballSkill";
 
 type PlayerProps = {
-  player: FibObject;
-  players: FibObject[];
+	player: FibObject;
+	players: FibObject[];
 };
 
 export function Player(props: PlayerProps) {
-  const { player, players } = props;
+	const { player, players } = props;
 
-  const playerMoveMutation = usePlayerMoveMutation();
-  const game = useStore(gameStore);
+	const playerMoveMutation = usePlayerMoveMutation();
+	const game = useStore(gameStore);
 
-  const scale = 32;
+	const scale = 32;
 
-  const top = player?.y * scale;
-  const left = player?.x * scale;
+	const top = player?.y * scale;
+	const left = player?.x * scale;
 
-  useInterval(() => {
-    window?.scrollTo({
-      top,
-      left,
-    });
-  }, 500);
+	useInterval(() => {
+		window?.scrollTo({
+			top,
+			left,
+		});
+	}, 500);
 
-  useEffect(() => {
-    const handleKeyDown = (keyboardEvent: KeyboardEvent) => {
-      // if(game.isLoading) {
-      //   return;
-      // }
+	useEffect(() => {
+		const handleKeyDown = (keyboardEvent: KeyboardEvent) => {
+			// if(game.isLoading) {
+			//   return;
+			// }
 
-      const keyboardEventKey = keyboardEvent.key.toUpperCase();
+			const keyboardEventKey = keyboardEvent.key.toUpperCase();
 
-      if (!["W", "S", "A", "D"].includes(keyboardEventKey)) {
-        return;
-      }
+			if (
+				![
+					"W",
+					"S",
+					"A",
+					"D",
+					"ArrowUp",
+					"ArrowDown",
+					"ArrowLeft",
+					"arrowRight",
+				].includes(keyboardEventKey)
+			) {
+				return;
+			}
 
-      playerMoveMutation.mutate({
-        player,
-        keyboardEventKey,
-      });
-    };
+			playerMoveMutation.mutate({
+				player,
+				keyboardEventKey,
+			});
+		};
 
-    window.addEventListener("keydown", handleKeyDown);
+		window.addEventListener("keydown", handleKeyDown);
 
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [player, game]);
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+		};
+	}, [player, game]);
 
-  if (game.isLoading && player?.fat !== game?.fat) {
-    // @ts-ignore
-    setGameStore({ isLoading: false, fat: player?.fat });
-  }
+	if (game.isLoading && player?.fat !== game?.fat) {
+		// @ts-ignore
+		setGameStore({ isLoading: false, fat: player?.fat });
+	}
 
-  return (
-    <>
-      <Blob side="right" {...player} x={left} y={top} enableDebug />
-      {/* <RangeTile top={top} left={left} /> */}
-      {game.isLoading ? (
-        <div className={$`abs top:${top - 20} left:${left - 35}`}>
-          <p>Loading...</p>
-        </div>
-      ) : null}
-      <Window player={player} players={players} />
-      {/* <EventLogs /> */}
-      {/* {game.events.map((e) => {
-        return {
-          fireball: (
-            <FireballSkill x={e.position.x} y={e.position.y} id={e.id} />
-          ),
-        }[e.type];
-      })} */}
-    </>
-  );
+	return (
+		<>
+			{game.isEnableRangeSkill ? (
+				<FireballRangeSkill top={top} left={left} />
+			) : null}
+			<Blob side="right" {...player} x={left} y={top} enableDebug />
+			{game.isLoading ? (
+				<div className={$`abs top:${top - 20} left:${left - 35}`}>
+					<p>Loading...</p>
+				</div>
+			) : null}
+			<Window player={player} players={players} />
+			{/* <EventLogs /> */}
+			{game.events.map((e) => {
+				return {
+					fireball: (
+						<FireballSkill x={e.position.x} y={e.position.y} id={e.id} />
+					),
+				}[e.type];
+			})}
+		</>
+	);
 }
