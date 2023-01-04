b7bef78e0849fe6b65e27f57c2853df1feb491e7
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2023-01-04 01:53:05 -0300

    chore: add hover

--- atoms/Button.tsx
+++ atoms/Button.tsx
@@ -8,7 +8,7 @@ export function Button(props: ButtonProps): JSX.Element {
 
 	return (
 		<button
-			className={$`py:18 px:70 rounded bg:#23dc79 box-shadow:0|0|3|5|#6508a6 color:black ${className}`}
+			className={$`py:18 px:70 rounded bg:#23dc79 bg:green-80:hover  box-shadow:0|0|3|5|#6508a6 color:black ${className}`}
 			{...rest}
 		>
 			{children}
