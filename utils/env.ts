567d742bc566a59e22601e7be0f558e92da6c5aa
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2023-01-04 01:57:30 -0300

    chore: create enum Server

--- utils/env.ts
+++ utils/env.ts
@@ -1,3 +1,9 @@
-// export const NODE_URL = "https://node-ase1.testnet.kindelia.org";
-export const NODE_URL = "https://node-sfo3.testnet.kindelia.org";
+enum Server {
+	Mainnet = "https://node.kindelia.org",
+	Testnet = "https://node-sfo3.testnet.kindelia.org",
+	SFO3 = "https://node-sfo3.testnet.kindelia.org",
+	ASE1 = "https://node-ase1.testnet.kindelia.org",
+}
+
+export const NODE_URL = Server.Testnet;
 export const NODE_SOCKET_URL = NODE_URL.replace("https://", "");
