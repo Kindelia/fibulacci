3a72ca4189a955f7e46fab20bb0ab7d937d482c0
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2023-01-04 01:54:00 -0300

    chore: update rainbowkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,49 @@
-import '@master/css';
-import '@master/keyframes.css';
-import '@master/normal.css';
+import "@master/css";
+import "@master/keyframes.css";
+import "@master/normal.css";
 
-import '@rainbow-me/rainbowkit/styles.css';
+import "@rainbow-me/rainbowkit/styles.css";
 
-import '../styles/globals.css';
+import "../styles/globals.css";
 
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
+import "../utils/webSocket";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+	[mainnet, polygon, optimism, arbitrum],
+	[alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()],
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "Fibulacci",
-  chains,
+	appName: "Fibulacci",
+	chains,
 });
 
 const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
+	autoConnect: true,
+	connectors,
+	provider,
 });
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <QueryClientProvider client={new QueryClient()}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider chains={chains}>
+				<QueryClientProvider client={new QueryClient()}>
+					<Component {...pageProps} />
+					<Toaster position="top-right" reverseOrder={false} />
+				</QueryClientProvider>
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
 }
 
 export default MyApp;
